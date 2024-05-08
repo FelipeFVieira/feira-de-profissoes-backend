fb9693376ea61a080c169e61a6a7a8387369b9ba
FelipeFVieira <devfelipefranca@gmail.com>
2024-05-07 22:27:38 -0300

    feat: add Spring Project

---
+++ presence/src/test/java/com/senaisuico/presence/PresenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senaisuico.presence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PresenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
